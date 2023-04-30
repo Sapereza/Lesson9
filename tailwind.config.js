e40bf6ec0fe18b6c6741b13750610d13fa953d12
Sapereza <s.perezanderson@gmail.com>
2023-04-30 19:12:26 -0400

    working on the about section. Added a resume.html for use in iframe on the about page. Working on width and layout of content on about page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./index.html","./about.html","./portfolio.html","./app.js","./contact.html"],
+  content: ["./src/**/*.{html,js}","./index.html","./about.html","./portfolio.html","./app.js","./contact.html","./resume.html"],
   theme: {
     extend: {},
   },

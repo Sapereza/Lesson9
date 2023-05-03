2f1118ab05eb5a00d1f7539ab28aa959eedb42a7
Sapereza <s.perezanderson@gmail.com>
2023-05-03 12:58:13 -0400

    got the download button working on the about page. There is some leftover code like id and such - plan is to edit these later and have the download button work through JS instead of the download attribute

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ function Menu(e){
 
     e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
 }
+
+//START of JS for Download button

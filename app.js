968132309b2366bff9b0e323c1e12a2ca496e36d
Sapereza <s.perezanderson@gmail.com>
2023-05-03 15:39:09 -0400

    set up the contact form so that it is ready to actually work. The messages do not actually go anywhere; however the code in the app.js file is set up so that the proper information can be input like a secure token to encrypt data or actual information to test locally only.

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ function Menu(e){
 }
 
 //START of JS for Download button
+//START OF JS FOR CONTACT SUBMIT BUTTON
+function sendEmail(){
+    Email.send({
+        SecureToken: "",
+        //After a securetoken has been entered above the next 3 lines can be removed.
+        //They are here for local testing purposes only
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+
+        To : 'them@website.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Inquiry",
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Successfully")
+    );
+}
